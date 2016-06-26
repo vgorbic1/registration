87011564e31192750ebf987a0bf9985e3f8baf4d
Vladislav Gorbich <vgorbic1@users.noreply.github.com>
2016-06-26 14:09:54 -0500

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Part of Registration process
+ * Complete registration by following the link in email
+ * Veryfy email with this file
+ *@author Vlad Gorbich
+ */
+require_once 'class.user.php';
+$user = new USER();
+
+if(empty($_GET['id']) && empty($_GET['code']))
+{
+	$user->redirect('index.php');
+}
+
+if(isset($_GET['id']) && isset($_GET['code']))
+{
+	$id = base64_decode($_GET['id']);
+	$code = $_GET['code'];
+	
+	$statusY = "Y";
+	$statusN = "N";
+	
+	$stmt = $user->runQuery("SELECT userID,userStatus FROM tbl_users WHERE userID=:uID AND tokenCode=:code LIMIT 1");
+	$stmt->execute(array(":uID"=>$id,":code"=>$code));
+	$row=$stmt->fetch(PDO::FETCH_ASSOC);
+	if($stmt->rowCount() > 0)
+	{
+		if($row['userStatus']==$statusN)
+		{
+			$stmt = $user->runQuery("UPDATE tbl_users SET userStatus=:status WHERE userID=:uID");
+			$stmt->bindparam(":status",$statusY);
+			$stmt->bindparam(":uID",$id);
+			$stmt->execute();	
+			
+			$msg = "
+		           <div class='alert alert-success'>
+					  You have been registered for NGC 2017.
+			       </div>
+			       ";	
+		}
+		else
+		{
+			$msg = "
+		           <div class='alert alert-danger'>
+				   <button class='close' data-dismiss='alert'>&times;</button>
+					  Your registeation has bean confirmed earlier.
+			       </div>
+			       ";
+		}
+	}
+	else
+	{
+		$msg = "
+		       <div class='alert alert-danger'>
+			   <button class='close' data-dismiss='alert'>&times;</button>
+			     No registration found
+			   </div>
+			   ";
+	}	
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Registration Confirmation</title>
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="assets/styles.css" rel="stylesheet" media="screen">
+     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+  </head>
+  <body id="login">
+    <div class="container">
+		<?php if(isset($msg)) { echo $msg; } ?>
+    </div> <!-- /container -->
+    <script src="vendors/jquery-1.9.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
